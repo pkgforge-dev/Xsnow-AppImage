a4fafd79aba213c7884414570969ecf558796d3d
Link4Electronics <link4electronics@gmail.com>
2026-03-21 07:27:02 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,21 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm atk cairo gsl libxpm
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building stable version of Xsnow..."
+echo "---------------------------------------------------------------"
+VERSION=3.9.0
+echo "$VERSION" > ~/version
+wget https://www.ratrabbit.nl/downloads/xsnow/xsnow-$VERSION.tar.gz
+tar -xvf ./xsnow-$VERSION.tar.gz
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cd ./xsnow-$VERSION
+./configure
+make -j$(nproc)
+mv -v src/xsnow ../AppDir/bin
