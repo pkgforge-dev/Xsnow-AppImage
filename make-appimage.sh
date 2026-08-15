8c084a9f80cb538c999dc00e4c1c251c6473e3c9
Link4Electronics <link4electronics@gmail.com>
2026-08-15 19:51:29 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q xsnow | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q xsnow | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -17,8 +17,6 @@ export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/xsnow
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
