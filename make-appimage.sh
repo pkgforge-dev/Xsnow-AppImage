380844462110f3642f748bb7ff956a54c9e35eac
Link4Electronics <link4electronics@gmail.com>
2026-08-29 17:52:05 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -20,7 +20,3 @@ quick-sharun /usr/bin/xsnow
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
