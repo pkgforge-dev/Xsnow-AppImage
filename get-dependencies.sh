30a13eb6ac71d947d88ba1c0b3b413068ff88003
Link4Electronics <link4electronics@gmail.com>
2026-08-30 08:25:07 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,12 +16,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package xsnow
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
 echo "Building Xsnow..."
 echo "---------------------------------------------------------------"
 VERSION=3.9.0
