06d059365455decc34fdff929f5e407f9a645a92
Link4Electronics <link4electronics@gmail.com>
2026-08-30 08:07:07 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,7 +18,6 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package xsnow
-make-aur-package opencv4
 
 # If the application needs to be manually built that has to be done down here
 
